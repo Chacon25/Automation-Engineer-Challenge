6810fd5d48e56b67eafa4e7ddda0d1120b39ff7b
FERNANDO NANDO CHACON MEDINA <marco.chacon@unitec.edu>
2020-08-16 16:20:21 -0600

    Calculate Economy and Long-Term

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -8,6 +8,10 @@ describe('Testing Parking Cost Calculator' ,()=>{
     beforeEach(()=>{
 
         cy.visit('http://www.shino.de/parkcalc/')
+        cy.on('uncauht:exception' , (err, runnable) => {
+            return false
+
+        })
 
     })
 
@@ -53,15 +57,10 @@ describe('Testing Parking Cost Calculator' ,()=>{
 
 
     it('Working in Valet Parking  ' , () => {
-       /*  VerifyCB.VerifyCB('Valet Parking')
-        VerifyTextBox.Verifytextbox('8/4/2020','8/4/2020','2:00', '3:00')
-        VerifyRB.VerifyRB('PM','PM')
-         */
-
-        // ParkingLot,StartingDate ,LeavingDate , StartingTime ,StartingTimeSet, LeavingTime , LeavingTimeSet
+      
         Calculate.Calculate('Valet Parking' , '8/4/2020' , '8/5/2020' , '3:00','PM' , '3:00', 'PM'  )
 
-        cy.get('[type="submit"]').click()
+       
         cy.get(cy.get('.SubHead > b').contains('$ 36.0'))
     })
 
@@ -69,9 +68,29 @@ describe('Testing Parking Cost Calculator' ,()=>{
 
         Calculate.Calculate('Short-Term Parking' , '8/4/2020' , '8/5/2020' , '3:00','PM' , '3:00', 'PM'  )
 
-        cy.get('[type="submit"]').click()
+    
         cy.get(cy.get('.SubHead > b').contains('$ 48.0'))
     })
+    it('Working in Economy Parking ' , ()=> {
+        Calculate.Calculate('Economy Parking', '3/4/2021' , '3/12/2021' , '1:00' ,  'AM' , '9:00' , 'PM')
+
+        cy.get(cy.get('.SubHead > b').contains('$ 72.0'))
+
+
+    })
+
+    it('Working in Long-Term Garage Parking ',()=> {
+
+        Calculate.Calculate('Long-Term Garage Parking', '5/5/2021' , '5/6/2021' , '2:00','AM' ,'12:00','AM' )
+        cy.get(cy.get('.SubHead > b').contains('$ 12.0'))
+
+    } )
+
+    it('Working in Long-Term Surface Parking' , () => {
+
+        Calculate.Calculate('Long-Term Surface Parking','5/5/2021' , '5/5/2021' , '1:00','AM' ,'2:00','PM')
+        cy.get(cy.get('.SubHead > b').contains('$ 10.0'))
+    } )
 
 
 
